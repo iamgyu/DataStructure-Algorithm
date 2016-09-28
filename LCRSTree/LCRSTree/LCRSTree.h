64be68366fb52a995cb5954d7629f6638ccc4415
iamgyu <iamgyu@naver.com>
2016-09-28 21:05:31 +0900

    LeftChild-RightSibling Tree - Finish

--- LCRSTree/LCRSTree/LCRSTree.h
+++ LCRSTree/LCRSTree/LCRSTree.h
@@ -1,6 +1,24 @@
 #ifndef LCRSTREE_H
 #define LCRSTREE_H
 
+#include <iostream>
+#include <stdlib.h>
 
+typedef char ElementType;
+
+typedef struct tagLCRSNode
+{
+	struct tagLCRSNode* LeftChild;
+	struct tagLCRSNode* RightSibling;
+
+	ElementType Data;
+}LCRSNode;
+
+LCRSNode* LCRS_CreateNode(ElementType NewData);						// 노드 생성 함수
+void LCRS_DestroyNode(LCRSNode* Node);								// 노드 소멸 함수
+void LCRS_DestroyTree(LCRSNode* Root);								// 트리 소멸 함수
+
+void LCRS_AddChildNode(LCRSNode* Parent, LCRSNode* Child);			// 자식 노드 추가 함수
+void LCRS_PrintTree(LCRSNode* Node, int Depth);					// 트리 출력 함수
 
 #endif
