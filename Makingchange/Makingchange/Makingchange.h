2369412d52c6aad7c1a46b2a90db3ecb84ec9c51
iamgyu <iamgyu@naver.com>
2016-10-11 12:07:33 +0900

    Makingchange-Finish

---
+++ Makingchange/Makingchange/Makingchange.h
@@ -0,0 +1,8 @@
+#ifndef MAKING_CHANGE_H
+#define MAKING_CHANGE_H
+
+void GetChange(int Price, int Pay, int CoinUnits[], int Change[], int Size);
+int CountCoins(int Amount, int CoinUnit);
+void PrintChange(int CoinUnits[], int Change[], int Size);
+
+#endif
