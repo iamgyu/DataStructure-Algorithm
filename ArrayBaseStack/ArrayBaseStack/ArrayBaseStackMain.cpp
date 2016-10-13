079d0c9f9dcaf7936f96c5082c7f8e4c1d8d7c8f
iamgyu <iamgyu@naver.com>
2016-10-13 22:24:24 +0900

    ArrayBaseStack-Finish(C Language DataStructure Book)

---
+++ ArrayBaseStack/ArrayBaseStack/ArrayBaseStackMain.cpp
@@ -0,0 +1,20 @@
+#include "ArrayBaseStack.h"
+#include <iostream>
+
+int main()
+{
+	// Stack의 생성 및 초기화
+	Stack stack;
+	StackInit(&stack);
+
+	// 데이터 넣기
+	SPush(&stack, 1); SPush(&stack, 2);
+	SPush(&stack, 3); SPush(&stack, 4);
+	SPush(&stack, 5);
+
+	// 데이터 꺼내기
+	while (!SIsEmpty(&stack))
+		printf("%d ", SPop(&stack));
+
+	return 0;
+}
